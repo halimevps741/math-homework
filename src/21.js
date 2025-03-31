fe919c3a9e506beda31aaee173835c6fa019cdc8
halimevps741 <halimevps741@gmail.com>
2025-03-31 03:27:10 +0000

    Add 21.js

---
+++ src/21.js
@@ -0,0 +1,10 @@
+// Node.js code example to calculate the factorial of a number using recursion
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+console.log(factorial(5)); // Output: 120
