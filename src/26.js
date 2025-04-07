2dc997336d86c54dcb26d6fec22520133e6b8743
halimevps741 <halimevps741@gmail.com>
2025-04-07 04:08:17 +0000

    Add 26.js

---
+++ src/26.js
@@ -0,0 +1,6 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Example usage
+console.log(getRandomInt(10, 20)); // Output: a random integer between 10 and 20
